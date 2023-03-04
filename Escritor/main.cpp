589755cd7c5568a9a8f1e391728951f0ae70c1c1
Mónica Castillo <monicavaleriamendoza@yahoo.com>
2023-03-04 00:46:03 -0600

    Creacion del segmento de memoria compartida entre procesos

--- Escritor/main.cpp
+++ Escritor/main.cpp
@@ -1,12 +1,21 @@
 #include "escritor.h"
-
 #include <QApplication>
+#include <QSharedMemory>
+#include <QDataStream>
 
+// Especie de clase para un Empleado
 struct empleado_tipo {
-int id;
-char nombreCompleto[50];
-float sueldo;
-int edad;
+    int id;
+    char nombreCompleto[50];
+    float sueldo;
+    int edad;
+};
+
+// Segmento con la data para la Memoria Compartida
+struct Segmento {
+    constexpr static int numMax_Empleados = 1000;
+    empleado_tipo empleados[numMax_Empleados];
+    int numEmpleados_Arreglo;
 };
 
 int main(int argc, char *argv[])
@@ -14,5 +23,22 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Escritor w;
     w.show();
+
+    // Llave para la Memoria Compartida
+    //const int sharedMemorySize = sizeof(Segmento);
+    const QString sharedMemoryKey = "MySharedMemory";
+    QSharedMemory sharedMemory(sharedMemoryKey);
+
+    if (!sharedMemory.isAttached() && !sharedMemory.attach()) {
+        qWarning("Unable to attach to shared memory segment: %s", sharedMemory.errorString().toUtf8().constData());
+        return 1;
+    }
+
+
+    Segmento* sharedMemoryPointer = (Segmento*) sharedMemory.data();
+    Segmento segment = *sharedMemoryPointer;
+
+    qDebug("Data read to shared memory: %d, %d", segment.numEmpleados_Arreglo, segment.numMax_Empleados);
+
     return a.exec();
 }
--- Lector/main.cpp
+++ Lector/main.cpp
@@ -1,12 +1,21 @@
 #include "lector.h"
-
 #include <QApplication>
+#include <QSharedMemory>
+#include <QDataStream>
 
+// Especie de clase para un Empleado
 struct empleado_tipo {
-int id;
-char nombreCompleto[50];
-float sueldo;
-int edad;
+    int id;
+    char nombreCompleto[50];
+    float sueldo;
+    int edad;
+};
+
+// Segmento con la data para la Memoria Compartida
+struct Segmento {
+    constexpr static int numMax_Empleados = 1000;
+    empleado_tipo empleados[numMax_Empleados];
+    int numEmpleados_Arreglo;
 };
 
 int main(int argc, char *argv[])
@@ -14,5 +23,31 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Lector w;
     w.show();
+
+    // Llave para la Memoria Compartida
+    const int sharedMemorySize = sizeof(Segmento);
+    const QString sharedMemoryKey = "MySharedMemory";
+    QSharedMemory sharedMemory(sharedMemoryKey);
+
+    //
+    Segmento segment;
+    //segment.numEmpleados_Arreglo = sizeof(segment.empleados);
+    segment.numEmpleados_Arreglo = 777;
+
+    if (!sharedMemory.create(sharedMemorySize)) {
+        qWarning("Unable to create shared memory segment: %s", sharedMemory.errorString().toUtf8().constData());
+        return 1;
+    }
+
+    if (!sharedMemory.isAttached() && !sharedMemory.attach()) {
+        qWarning("Unable to attach to shared memory segment: %s", sharedMemory.errorString().toUtf8().constData());
+        return 1;
+    }
+
+    Segmento* sharedMemoryPointer = (Segmento*) sharedMemory.data();
+    memcpy(sharedMemoryPointer, &segment, sharedMemorySize);
+
+    qDebug("Data written to shared memory: %d, %d", segment.numEmpleados_Arreglo, segment.numMax_Empleados);
+
     return a.exec();
 }
