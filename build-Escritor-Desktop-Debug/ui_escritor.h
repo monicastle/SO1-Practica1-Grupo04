471a20af979ce3f97826dd189e80c3a211d6608b
SahoryHerrera <73321741+SahoryHerrera@users.noreply.github.com>
2023-03-04 01:50:42 -0600

    Formularios
    1.1

--- build-Escritor-Desktop-Debug/ui_escritor.h
+++ build-Escritor-Desktop-Debug/ui_escritor.h
@@ -10,10 +10,13 @@
 #define UI_ESCRITOR_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QIcon>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -24,6 +27,12 @@ class Ui_Escritor
 public:
     QWidget *centralwidget;
     QLabel *label;
+    QLineEdit *lineEdit;
+    QLineEdit *lineEdit_2;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -31,16 +40,41 @@ public:
     {
         if (Escritor->objectName().isEmpty())
             Escritor->setObjectName(QString::fromUtf8("Escritor"));
-        Escritor->resize(531, 404);
+        Escritor->resize(611, 446);
         centralwidget = new QWidget(Escritor);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         label = new QLabel(centralwidget);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(110, 50, 67, 17));
+        label->setGeometry(QRect(0, -10, 611, 431));
+        label->setPixmap(QPixmap(QString::fromUtf8(":/Imagenes/Escritor.png")));
+        label->setScaledContents(true);
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        lineEdit->setGeometry(QRect(390, 80, 151, 21));
+        lineEdit_2 = new QLineEdit(centralwidget);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+        lineEdit_2->setGeometry(QRect(390, 120, 151, 21));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(550, 70, 41, 31));
+        pushButton->setStyleSheet(QString::fromUtf8("border: none;"));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8("../../../../Descargas/buscar(1).png"), QSize(), QIcon::Normal, QIcon::Off);
+        pushButton->setIcon(icon);
+        pushButton->setIconSize(QSize(30, 30));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(320, 200, 131, 25));
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setGeometry(QRect(470, 200, 131, 25));
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+        pushButton_4->setGeometry(QRect(400, 250, 131, 25));
         Escritor->setCentralWidget(centralwidget);
         menubar = new QMenuBar(Escritor);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 531, 22));
+        menubar->setGeometry(QRect(0, 0, 611, 22));
         Escritor->setMenuBar(menubar);
         statusbar = new QStatusBar(Escritor);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -54,7 +88,11 @@ public:
     void retranslateUi(QMainWindow *Escritor)
     {
         Escritor->setWindowTitle(QCoreApplication::translate("Escritor", "Escritor", nullptr));
-        label->setText(QCoreApplication::translate("Escritor", "TextLabel", nullptr));
+        label->setText(QString());
+        pushButton->setText(QString());
+        pushButton_2->setText(QCoreApplication::translate("Escritor", "Generar nuevo ID", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("Escritor", "Modificar", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("Escritor", "Eliminar", nullptr));
     } // retranslateUi
 
 };
--- build-Lector-Desktop-Debug/ui_lector.h
+++ build-Lector-Desktop-Debug/ui_lector.h
@@ -10,11 +10,17 @@
 #define UI_LECTOR_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QIcon>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpinBox>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableWidget>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -24,6 +30,14 @@ class Ui_Lector
 public:
     QWidget *centralwidget;
     QLabel *label;
+    QSpinBox *spinBox;
+    QSpinBox *spinBox_2;
+    QLineEdit *lineEdit;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QTableWidget *tableWidget;
+    QLineEdit *lineEdit_2;
+    QPushButton *pushButton_3;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -31,16 +45,50 @@ public:
     {
         if (Lector->objectName().isEmpty())
             Lector->setObjectName(QString::fromUtf8("Lector"));
-        Lector->resize(548, 379);
+        Lector->resize(599, 510);
         centralwidget = new QWidget(Lector);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         label = new QLabel(centralwidget);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(30, 30, 67, 17));
+        label->setGeometry(QRect(0, 0, 601, 491));
+        label->setPixmap(QPixmap(QString::fromUtf8(":/Imagenes/Lector.png")));
+        label->setScaledContents(true);
+        spinBox = new QSpinBox(centralwidget);
+        spinBox->setObjectName(QString::fromUtf8("spinBox"));
+        spinBox->setGeometry(QRect(170, 120, 71, 26));
+        spinBox_2 = new QSpinBox(centralwidget);
+        spinBox_2->setObjectName(QString::fromUtf8("spinBox_2"));
+        spinBox_2->setGeometry(QRect(390, 120, 71, 26));
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        lineEdit->setGeometry(QRect(170, 170, 291, 25));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(480, 120, 51, 25));
+        pushButton->setStyleSheet(QString::fromUtf8("border: none;"));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8("../../../../Descargas/buscar.png"), QSize(), QIcon::Normal, QIcon::Off);
+        pushButton->setIcon(icon);
+        pushButton->setIconSize(QSize(25, 25));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(480, 170, 51, 25));
+        pushButton_2->setStyleSheet(QString::fromUtf8("border: none;"));
+        pushButton_2->setIcon(icon);
+        pushButton_2->setIconSize(QSize(25, 25));
+        tableWidget = new QTableWidget(centralwidget);
+        tableWidget->setObjectName(QString::fromUtf8("tableWidget"));
+        tableWidget->setGeometry(QRect(50, 210, 501, 211));
+        lineEdit_2 = new QLineEdit(centralwidget);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+        lineEdit_2->setGeometry(QRect(190, 440, 231, 25));
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setGeometry(QRect(440, 440, 89, 25));
         Lector->setCentralWidget(centralwidget);
         menubar = new QMenuBar(Lector);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 548, 22));
+        menubar->setGeometry(QRect(0, 0, 599, 22));
         Lector->setMenuBar(menubar);
         statusbar = new QStatusBar(Lector);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -54,7 +102,10 @@ public:
     void retranslateUi(QMainWindow *Lector)
     {
         Lector->setWindowTitle(QCoreApplication::translate("Lector", "Lector", nullptr));
-        label->setText(QCoreApplication::translate("Lector", "Prueba", nullptr));
+        label->setText(QString());
+        pushButton->setText(QString());
+        pushButton_2->setText(QString());
+        pushButton_3->setText(QCoreApplication::translate("Lector", "Calcular", nullptr));
     } // retranslateUi
 
 };
