43b0abcfad420528c29d8ee5b41bc0a9086c48ce
jamd10 <73321263+jamd10@users.noreply.github.com>
2023-03-06 14:15:37 -0600

    Update
    solucion parent y ui

--- build-Escritor-Desktop-Debug/moc_escritor.cpp
+++ build-Escritor-Desktop-Debug/moc_escritor.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Escritor_t {
-    QByteArrayData data[6];
-    char stringdata0[93];
+    QByteArrayData data[5];
+    char stringdata0[71];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,13 +36,11 @@ QT_MOC_LITERAL(0, 0, 8), // "Escritor"
 QT_MOC_LITERAL(1, 9, 17), // "on_btn_id_clicked"
 QT_MOC_LITERAL(2, 27, 0), // ""
 QT_MOC_LITERAL(3, 28, 18), // "on_btn_mod_clicked"
-QT_MOC_LITERAL(4, 47, 23), // "on_btn_eliminar_clicked"
-QT_MOC_LITERAL(5, 71, 21) // "on_btn_buscar_clicked"
+QT_MOC_LITERAL(4, 47, 23) // "on_btn_eliminar_clicked"
 
     },
     "Escritor\0on_btn_id_clicked\0\0"
-    "on_btn_mod_clicked\0on_btn_eliminar_clicked\0"
-    "on_btn_buscar_clicked"
+    "on_btn_mod_clicked\0on_btn_eliminar_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -52,7 +50,7 @@ static const uint qt_meta_data_Escritor[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -60,13 +58,11 @@ static const uint qt_meta_data_Escritor[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   34,    2, 0x08 /* Private */,
-       3,    0,   35,    2, 0x08 /* Private */,
-       4,    0,   36,    2, 0x08 /* Private */,
-       5,    0,   37,    2, 0x08 /* Private */,
+       1,    0,   29,    2, 0x08 /* Private */,
+       3,    0,   30,    2, 0x08 /* Private */,
+       4,    0,   31,    2, 0x08 /* Private */,
 
  // slots: parameters
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -83,7 +79,6 @@ void Escritor::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
         case 0: _t->on_btn_id_clicked(); break;
         case 1: _t->on_btn_mod_clicked(); break;
         case 2: _t->on_btn_eliminar_clicked(); break;
-        case 3: _t->on_btn_buscar_clicked(); break;
         default: ;
         }
     }
@@ -119,13 +114,13 @@ int Escritor::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 3;
     }
     return _id;
 }
